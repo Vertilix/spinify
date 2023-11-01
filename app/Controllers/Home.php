66d2cdb048cd1774bea691e940f05f34a4db7ff9
Vertilix <vertilix0@gmail.com>
2023-11-01 16:56:28 +0100

    Update load libraries location and improve code a bit

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,7 +6,7 @@ class Home extends BaseController
 {
     public function index(): string
     {
-        return view('index');
+        return view('templates/navbar.html')
+        . view('index');
     }
-
 }
--- app/Controllers/Login.php
+++ app/Controllers/Login.php
@@ -4,13 +4,9 @@ namespace App\Controllers;
 
 class Login extends BaseController
 {
-    
-    public function login(): string {
-        $session = \Config\Services::session();
-        $db = \Config\Database::connect();
-        
+    public function login(): string {        
         $rules = [
-            'username'     => 'required|max_length[200]|min_length[3]',
+            'username'     => 'required|max_length[50]|min_length[3]',
             'password'  => 'required|max_length[200]|min_length[6   ]',
         ];
 
@@ -21,14 +17,11 @@ class Login extends BaseController
 
         $validData = $this->validator->getValidated();
         $sql       = "SELECT * FROM `users` WHERE `username` = ?";
-        $query     = $db->query($sql, [$validData['username']]);
+        $query     = $this->db->query($sql, [$validData['username']]);
         $results   = $query->getRowArray();
 
         if(password_verify($validData['password'], $results['password'])){
-            if($session->get('user')){
-                $session->remove('user');
-            }
-            $session->set('user', $results);
+            $this->session->set('user', $results);
             return view('login/lSuccess', $results);
         }else{
             session()->setFlashdata('error', 'Username or password is incorrect');
@@ -36,11 +29,7 @@ class Login extends BaseController
         }
     }
     
-    public function register(){
-        $session = \Config\Services::session();
-        $db      = \Config\Database::connect();
-        
-        
+    public function register(): string{
         $rules = [
             'username'  => 'required|max_length[50]|min_length[3]',
             'email'     => 'required|max_length[200]|min_length[3]',
@@ -52,12 +41,11 @@ class Login extends BaseController
             view('login/register');
         }
 
-        $validData = $this->validator->getValidated();
+        $validData      = $this->validator->getValidated();
         $hashedPassword = password_hash($validData['password'], PASSWORD_DEFAULT);
-        $sql = "INSERT INTO `users`(`id`, `username`, `password`, `email`, `role`) VALUES (NULL, ?, ?, ?, 'guest')";
-        $db->query($sql, [$validData['username'], $hashedPassword, $validData['email']]);
+        $sql            = "INSERT INTO `users`(`id`, `username`, `password`, `email`, `role`) VALUES (NULL, ?, ?, ?, 'guest')";
+        $this->db->query($sql, [$validData['username'], $hashedPassword, $validData['email']]);
 
         return view('login/rSuccess');
     }
-
 }
