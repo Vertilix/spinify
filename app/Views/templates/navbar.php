2b1f77237c74616daaf42a95be41ce4b1b6ae828
Vertilix <vertilix0@gmail.com>
2023-11-09 10:46:55 +0100

    Fix isset statement & added more css

--- app/Views/templates/navbar.php
+++ app/Views/templates/navbar.php
@@ -1,10 +1,10 @@
 <div class="navbar">
     <h1 class="logo" style="color: #FFFFFF;">Spinify</h1>
     <a class="item" href="">Premium</a>
-    <a class="item">Download</a>
-    <a class="item">Support</a>
-    <?php if($_SESSION['user']) { ?>
-        <a href="/login" class="item">Login</a>
+    <a class="item" href="">Download</a>
+    <a class="item" href="">Support</a>
+    <?php if(!isset($_SESSION['user'])) { ?>
+        <a href="/login" class="item" style="padding-left:5px; border-left: 5px solid white;">Login</a>
         <a href="/login/register" class="item">Sign up</a>
     <?php } else { ?>
         <a href="/login" class="item">Logout</a>
