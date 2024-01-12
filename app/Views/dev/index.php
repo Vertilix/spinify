781b7aaa427a8df12807a9565b0d0112fcc42283
Mike <mike.van.pol@student.gildeopleidingen.nl>
2024-01-12 19:36:49 +0100

    added dev css

---
+++ app/Views/dev/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link type="text/css" rel="stylesheet" href="style.css">
+    <link type="text/css" rel="stylesheet" href="index.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="flex">
+        <div class="sidebar-container">
+            <div class="sidebar-div" style="height:40%">
+                <div class="sidebar-background">
+                    <div class="nav-brand">
+                        <img src="https://via.placeholder.com/50" alt="">
+                        <h2>Spinify</h2>
+                    </div>
+                    <div class="nav-brand">
+                        <img src="https://via.placeholder.com/50" alt="">
+                        <h2>Spinify</h2>
+                    </div>
+                    <div class="nav-brand">
+                        <img src="https://via.placeholder.com/50" alt="">
+                        <h2>Spinify</h2>
+                    </div>
+                </div>
+            </div>
+            <div class="sidebar-div" style="height:100%">
+                asdhsald
+            </div>
+        </div>
+        <div class="flex-main">
+            ahdklsad
+        </div>
+    </div>
+
+</body>
+</html>
