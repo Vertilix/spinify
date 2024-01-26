fa5c123620eda5e485532ce1fa850af576f808bc
Mike <mike.van.pol@student.gildeopleidingen.nl>
2024-01-26 12:03:08 +0100

    Update dev css

--- app/Views/dev/index.php
+++ app/Views/dev/index.php
@@ -12,34 +12,43 @@
         <div class="sidebar">
             <div class="navbar">
                 <div>
-                    <img class="logo" src="logo2.png" alt="">
+                    <img class="nav-logo" src="svgs/spinify.svg " alt="">
                 </div>
-                <a href="#" class="a-hover">
+                <a href="#" class="nav-a-hover">
                     <div>
-                        <img src="home.svg" width="35" height="35">
+                        <img src="svgs/home.svg" width="35" height="35">
                         <p>Home</p>
                     </div>
                 </a>
-                <a href="#" class="a-hover">
+                <a href="#" class="nav-a-hover">
                     <div>
-                        <img src="search.svg" width="35" height="35">
+                        <img src="svgs/search.svg" width="35" height="35">
                         <p>Search</p>
                     </div>
                 </a>
             </div>
             <div class="library">
                 <div class="lib-top">
-                    <a href="#" class="a-hover">Bibliotheek</a>
-                    <a href="#" class="a-hover">+</a>
+                    <a href="#" class="nav-a-hover">
+                        <div class="library-flex">
+                            <img src="svgs/library.svg" width="35" height="35">
+                            <p>Library</p>
+                        </div>
+                    </a>
+                    <a href="#" class="nav-a-hover">+</a>
                 </div>
-                <a href="#">Library</a>
-                <a href="#">Contact</a>
             </div>
         </div>
         <div class="main">
-            <a href="#">Home</a>
-            <a href="#">Library</a>
-            <a href="#">Contact</a>
+            <div class="main-top">
+                <form class="main-search">
+                    <input type="text" placeholder="Search" class="main-search-text">
+                    <input type="image" src="svgs/search.svg" width="35" height="35" value="Search">
+                </form>
+                <a href="#" class="nav-a-hover">Home</a>
+                <a href="#" class="nav-a-hover">Library</a>
+                <a href="#" class="nav-a-hover">Contact</a>
+            </div>
         </div>
     </div>
 
